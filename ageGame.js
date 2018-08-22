f5bc753994c821cc16198b38840174c7347cf276
Pavan Hassamal <phass001@gmail.com>
2018-08-21 21:14:53 -0700

    age game with JS

---
+++ ageGame.js
@@ -0,0 +1,13 @@
+var age = Number(prompt("What is your age"));
+
+if(age < 0) {
+alert("come back once you're out of the womb")
+};
+
+if(age === 21) {
+alert("happy 21'st birthday!")
+} ;
+
+if(age % Math.sqrt(age) === 0) {
+alert("your age is a perfect square")
+};
